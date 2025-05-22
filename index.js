76e4abcca6d5740b030a84d3ba198a76c249da51
Om Amar <emailomamar@gmail.com>
2025-05-23 05:16:10 +0530

    updated it for CORS

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const but = document.getElementById('change');
 async function func(name) {
     let data = {}
 
-    // const response = await fetch(`https://codeforces-rating-predictor-two.vercel.app/get/${name}`,{mode: 'no-cors'});
-    // data = await response.json();
+    const response = await fetch(`https://codeforces-rating-predictor-two.vercel.app/get/${name}`);
+    data = await response.json();
 
     // console.log(data);
-    data = {"prediction":1573.4168290030898,"user_data":{"avatar":"https://userpic.codeforces.org/3664768/avatar/5f67dde9290690e4.jpg","city":"Mumbai","contribution":44,"country":"India","firstName":"Om","friendOfCount":194,"handle":"DeadMan69","lastName":"Amar","lastOnlineTimeSeconds":1736675465,"maxRank":"candidate master","maxRating":1990,"organization":"Indian Institute of Information Technology, Nagpur","rank":"candidate master","rank_color":"#800080","rating":1969,"registrationTimeSeconds":1701795349,"titlePhoto":"https://userpic.codeforces.org/3664768/title/1d6b743dc7247966.jpg"}} 
+    // data = {"prediction":1573.4168290030898,"user_data":{"avatar":"https://userpic.codeforces.org/3664768/avatar/5f67dde9290690e4.jpg","city":"Mumbai","contribution":44,"country":"India","firstName":"Om","friendOfCount":194,"handle":"DeadMan69","lastName":"Amar","lastOnlineTimeSeconds":1736675465,"maxRank":"candidate master","maxRating":1990,"organization":"Indian Institute of Information Technology, Nagpur","rank":"candidate master","rank_color":"#800080","rating":1969,"registrationTimeSeconds":1701795349,"titlePhoto":"https://userpic.codeforces.org/3664768/title/1d6b743dc7247966.jpg"}} 
 
     const user_data = data['user_data'];
     const prediction = data['prediction'];
@@ -66,6 +66,6 @@ async function func(name) {
 if (but) {
     but.addEventListener('click', function () {
         const name = document.getElementById('name').value;
-        func("DeadMan69");
+        func(name);
     });
 }
